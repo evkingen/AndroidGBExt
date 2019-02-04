d333271117884674cd61104a1e59aeb7da9baf41
Georgiy Merkulov <zlock14@gmail.com>
2019-02-04 15:34:15 +0300

    Обращение к API github. Получение данных о пользователе и списке его репозиториев.

--- app/src/main/java/com/example/alohagoha/androidgbexthomework/mvp/model/entity/RepositoryDTO.java
+++ app/src/main/java/com/example/alohagoha/androidgbexthomework/mvp/model/entity/RepositoryDTO.java
@@ -4,7 +4,37 @@ import com.google.gson.annotations.Expose;
 
 public class RepositoryDTO {
     @Expose
+    private String id;
+    @Expose
     private String name;
+    @Expose
+    private OwnerDTO owner;
+
+    public RepositoryDTO() {
+
+    }
+
+    public RepositoryDTO(String id, String name, OwnerDTO owner) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public OwnerDTO getOwner() {
+        return owner;
+    }
+
+    public void setOwner(OwnerDTO owner) {
+        this.owner = owner;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -13,4 +43,5 @@ public class RepositoryDTO {
     public void setName(String name) {
         this.name = name;
     }
+
 }
--- app/src/main/java/com/example/alohagoha/androidgbexthomework/mvp/model/entity/UserDTO.java
+++ app/src/main/java/com/example/alohagoha/androidgbexthomework/mvp/model/entity/UserDTO.java
@@ -2,6 +2,9 @@ package com.example.alohagoha.androidgbexthomework.mvp.model.entity;
 
 import com.google.gson.annotations.Expose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDTO {
 
     @Expose
@@ -9,6 +12,37 @@ public class UserDTO {
 
     @Expose
     private String avatarUrl;
+
+    @Expose
+    private String reposUrl;
+
+    private List<RepositoryDTO> repos = new ArrayList<>();
+
+    public UserDTO() {
+
+    }
+
+    public UserDTO(String login, String avatarUrl, String reposUrl) {
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.reposUrl = reposUrl;
+    }
+
+    public List<RepositoryDTO> getRepos() {
+        return repos;
+    }
+
+    public void setRepos(List<RepositoryDTO> repos) {
+        this.repos = repos;
+    }
+
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    public void setReposUrl(String reposUrl) {
+        this.reposUrl = reposUrl;
+    }
 
     public String getAvatarUrl() {
         return avatarUrl;
